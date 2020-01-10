ad37bb4b645c1b96f68bfe09b902ba974fb81c17
Nikita <nikita-makarevich@mail.ru>
2020-01-10 22:46:36 +0300

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='smartbot',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Nikita1801',
+    author_email='nikita-makarevich@mail.ru',
+    description=''
+)
